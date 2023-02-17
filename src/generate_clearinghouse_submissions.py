4e1570f5475362b020249cb17a679ccd48c0985d
woollard <woollard@ebi.ac.uk>
2023-02-17 09:22:09 +0000

    initial new Sample object created

--- src/generate_clearinghouse_submissions.py
+++ src/generate_clearinghouse_submissions.py
@@ -10,6 +10,41 @@ import json
 from icecream import ic
 import os.path
 
+SavedDataStructures = {}
+
+
+class Sample:
+  def __init__(self):
+
+    self.assertionAdditionalInfo = None
+    #                           'assertionEvidences': [{'label': ''}],
+    self.assertionEvidences = None
+    self.assertionSource = None
+    self.attributeDelete = "true"
+    self.attributePost = None
+    self.attributePre = None
+    self.providerName = "European Nucleotide Archive"
+    self.providerUrl = "https://www.ebi.ac.uk/ena/browser/home"
+    self.recordId = None
+    self.recordType = 'sample'
+
+
+  def get_filled_dict(self):
+    my_dict = {}
+    my_dict['assertionAdditionalInfo'] = self.assertionAdditionalInfo
+    my_dict['assertionSource'] = self.assertionSource
+    my_dict['attributeDelete'] = self.attributeDelete
+    my_dict['attributePost'] = self.attributePost
+    my_dict['attributePre'] = self.attributePre
+    my_dict['providerName'] = self.providerName
+    my_dict['providerUrl'] = self.providerUrl
+    my_dict['recordId'] = self.recordId
+    my_dict['recordType'] = self.recordType
+
+    return my_dict
+
+
+
 
 
 def _get_template(template_type):
@@ -34,17 +69,18 @@ def _get_template(template_type):
 
     return(empty_dict)
 
+
 def do_format(test_status):
     sample_dict = _get_template("sample")
     ic(sample_dict['curations'])
     curations = sample_dict['curations']
     ic(curations)
-    ic(curations[0].keys())
+    curation_template_record = curations[0].copy()
+    ic(curation_template_record)
 
-    curations[0]['providerName'] = 'EBI ENA'
-    'providerUrl': '',
-
-
+    curation_count = 0
+    record = Sample()
+    ic(record.get_filled_dict())
 
 
 

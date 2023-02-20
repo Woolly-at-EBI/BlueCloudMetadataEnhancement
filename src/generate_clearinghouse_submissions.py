99fc7030f9e71a29f483f91afa2fbfddad71d08d
woollard <woollard@ebi.ac.uk>
2023-02-20 17:02:33 +0000

    collating all the curated JSONs ready for submission

--- src/generate_clearinghouse_submissions.py
+++ src/generate_clearinghouse_submissions.py
@@ -36,11 +36,12 @@ def demo_format(test_status):
     ic(my_record.get_filled_dict())
 
 def process_confidence_fields(df_merge_combined_tax, analysis_dir):
-    """
+    """process_confidence_fields for marine and terrestrial
+    generating JSON to have curated.
 
     :param df_merge_combined_tax:
     :param analysis_dir:
-    :return:
+    :return: list of JSON's to curate
     """
     ic(df_merge_combined_tax.head())
     ic(df_merge_combined_tax.columns)
@@ -77,15 +78,23 @@ def process_confidence_fields(df_merge_combined_tax, analysis_dir):
     ic(df_specific.head(5))
     ic(df_specific['json_col'].head())
 
+    local_list = df_specific['json_col'].values.tolist()
+    # remove empty list items
+    local_list = [i for i in local_list if i]
+
+    return local_list
+
 def process_eez_fields(df_merge_sea_ena):
     """process_eez_fields
-
+        process all the EEZ relevant fields and return a list of sample curations in JSON format to make.
     :param df_gps:
-    :return:
+    :return: curation_list
     """
     ic()
     ic(df_merge_sea_ena.sample(n=10))
     ic(df_merge_sea_ena.columns)
+
+    curation_list = []
 
     def createSubmissionsJson(row):
         my_record = NewSampleCuration(useENAAutoCurationValues = True)
@@ -96,24 +105,44 @@ def process_eez_fields(df_merge_sea_ena):
         print(my_record.get_filled_json())
         return my_record.get_filled_json()
 
-    curations2add = ['EEZ:GEONAME', 'EEZ:TERRITORY1', 'EEZ:TERRITORY2', 'EEZ:SOVEREIGN1', 'EEZ:SOVEREIGN2',\
+    #comparing the first value with the rest, as want to know if all values are the same
+    def is_unique(s):
+        a = s.to_numpy()  # s.values (pandas<0.24)
+        return (a[0] == a).all()
+
+    curation_types2add = ['EEZ:GEONAME', 'EEZ:TERRITORY1', 'EEZ:TERRITORY2', 'EEZ:SOVEREIGN1', 'EEZ:SOVEREIGN2',\
                      'EEZ:MRGID', 'EEZ:MRGID_TER1', 'EEZ:MRGID_TER2', 'EEZ:MRGID_SOV1', 'EEZ:MRGID_SOV2']
-    df_specific = df_merge_sea_ena.query('eez_category == "EEZ"').head(1)
+    df_specific = df_merge_sea_ena.query('eez_category == "EEZ"').head(2)
     assertionAdditionalInfo = "Confidence:high; evidence:GPS coordinate in EEZ shapefile"
-    for dom_type in curations2add:
+    for dom_type in curation_types2add:
         (dummy, field) = dom_type.split(":")
         ic(field)
-        if field in ['GEONAME', 'TERRITORY1', 'TERRITORY2', 'SOVEREIGN1', 'SOVEREIGN2']:
-            df_specific['json_col'] = df_specific.apply(createSubmissionsJson, axis=1)
-            df_specific.loc[df_specific[field].isnull(), 'json_col'] = ""
-        else:
-            df_specific['json_col'] = df_specific.apply(createSubmissionsJson, axis=1)
-            #below could not see an elegant way of dealing when MRGID's are 0.
-            df_specific.loc[df_specific[field] > 0, 'json_col'] = ""
 
-    #to do, capture valid curation, from json_col each time
-        ic(df_specific.head())
-        ic(df_specific['json_col'].head(3))
+        if(df_specific[field].isnull().all() or is_unique(df_specific[field])):
+            ic("field all values null or 0")
+            #thus do not need
+        else:
+            #during the below some empty "" values are created in json_col
+            if field in ['GEONAME', 'TERRITORY1', 'TERRITORY2', 'SOVEREIGN1', 'SOVEREIGN2']:
+                df_specific['json_col'] = df_specific.apply(createSubmissionsJson, axis=1)
+                df_specific.loc[df_specific[field].isnull(), 'json_col'] = ""
+            else:
+                df_specific['json_col'] = df_specific.apply(createSubmissionsJson, axis=1)
+                #earlier code filled in empty MRGID values to 0. Could not see an efficient way to stop doing an apply
+                # on those, hence the below is necessary.
+                df_specific.loc[df_specific[field] > 0, 'json_col'] = ""
+
+            #to do, capture valid curation, from json_col each time
+            ic(df_specific.head())
+            ic(df_specific['json_col'].head(3))
+            local_list = df_specific['json_col'].values.tolist()
+
+            #remove empty list items
+            local_list = [i for i in local_list if i]
+            curation_list.extend(local_list)
+            ic(len(curation_list))
+
+    return curation_list
 
 def merge_sea_ena(hit_dir):
     """merge_sea_ena
@@ -132,29 +161,42 @@ def merge_sea_ena(hit_dir):
     return df_merge_sea_ena
 
 
+def submit_curations(full_curation_list):
+    """
+
+    :param full_curation_list:
+    :return:
+    """
+    ic()
+    ic(len(full_curation_list))
+    ic("submit_curations TBD")
+
 def main():
     test_status = True
     (hit_dir, shape_dir, sample_dir, analysis_dir, plot_dir, taxonomy_dir) = get_directory_paths()
 
     df_merged_ena_sea = merge_sea_ena(hit_dir)
     df_merged_ena_sea = df_merged_ena_sea.query('eez_category == "EEZ"')
+    full_curation_list = []
 
     #demo_format(test_status)
     # in_file = analysis_dir + 'all_ena_gps_tax_combined.tsv'
     # df_gps = pd.read_csv(in_file, sep='\t', nrows =100)
-    process_eez_fields(df_merged_ena_sea)
+    local_curation_list = process_eez_fields(df_merged_ena_sea)
+    full_curation_list.extend(local_curation_list)
 
-    sys.exit()
+    #sys.exit()
 
     pickle_file = analysis_dir + 'merge_combined_tax_all_with_confidence.pickle'
     # df_merge_combined_tax = get_pickleObj(pickle_file)
     # df_merge_combined_tax = df_merge_combined_tax.head(100000).query('taxonomic_source == "metagenome"')
     #put_pickleObj2File(df_merge_combined_tax, "./tmp.pickle")
-    df_merge_combined_tax = get_pickleObj("./tmp.pickle")
-    process_confidence_fields(df_merge_combined_tax, analysis_dir)
 
+    #df_merge_combined_tax = get_pickleObj("./tmp.pickle")
+    #local_curation_list = process_confidence_fields(df_merge_combined_tax, analysis_dir)
+    #full_curation_list.extend(local_curation_list)
 
-
+    submit_curations(full_curation_list)
 
 if __name__ == '__main__':
     ic()

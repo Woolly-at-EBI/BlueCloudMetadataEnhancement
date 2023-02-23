fb1491deabf5ec365c3905a2e0d79d97f0865bce
woollard <woollard@ebi.ac.uk>
2023-02-23 10:11:46 +0000

    investigated collection dates

--- src/general_analysis.sh
+++ src/general_analysis.sh
@@ -20,6 +20,33 @@ raw_sample_file=$samples_dir/sample_much_raw.tsv
 coordinates_file=$samples_dir/all_sample_lat_longs_present.tsv
 
 
+function analyse_collection_dates () {
+  echo "Granularity of GPS coordinates"
+  echo "Total number of ENA samples in $raw_sample_file"
+  #tail +2 $raw_sample_file | wc -l
+
+  field="collection_date"
+  echo " For sample $field"
+  echo ""
+  tmp_dates_file=$samples_dir"tmp_dates.txt"
+  echo $tmp_dates_file
+  #cut -f5 $raw_sample_file | tail +2 | sed '/^$/d' > $tmp_dates_file
+  echo "Total number of non-null for "$field
+  wc -l $tmp_dates_file
+  echo "total samples <1900-01-01 in "$tmp_dates_file
+  awk '$1<1900-01-01' $tmp_dates_file| wc -l
+  echo "  examples"
+  awk '$1<1900-01-01' $tmp_dates_file| uniq| head -5
+  echo "total samples >2024-01-01 in "$tmp_dates_file
+  awk '$1>2024-01-01' $tmp_dates_file | wc -l
+  echo "  examples"
+  awk '$1>2024-01-01' $tmp_dates_file| uniq| grep -Ev '(2022|2023)' | head -5
+
+  #investigating a few
+  #head -1 sample_much_raw.tsv > dodgy_dates_tmp.tx
+  #egrep -E '(1897-10-01|0201-03-03|0514-01-01|1300-01-01|1777-08-03)' $raw_sample_file >> dodgy_dates_tmp.txt
+}
+
 function analyse_all_lat_lon () {
   echo "Granularity of GPS coordinates"
   echo "Total number of ENA samples:"
@@ -39,5 +66,6 @@ function analyse_all_lat_lon () {
 
 }
 
+analyse_collection_dates
 
-analyse_all_lat_lon
+# analyse_all_lat_lon

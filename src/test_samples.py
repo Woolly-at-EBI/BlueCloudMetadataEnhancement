2a7d98753f52d90b55cec224e51313ec476f4cc3
woollard <woollard@ebi.ac.uk>
2023-03-24 08:41:36 +0000

    initial

---
+++ src/test_samples.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Script of 'test_samples.py' is to '
+
+___author___ = "woollard@ebi.ac.uk"
+___start_date___ = 2023-03-24
+__docformat___ = 'reStructuredText'
+
+"""
+
+
+from icecream import ic
+
+import argparse
+
+def first_func():
+    """"""
+
+    ic()
+
+def main():
+    first_func()
+
+if __name__ == '__main__':
+    ic()
+
+

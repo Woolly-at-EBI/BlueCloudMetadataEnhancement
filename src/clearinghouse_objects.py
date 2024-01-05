57db843e0e46933dfbaa66f62e9a9246764197cd
woollard <woollard@ebi.ac.uk>
2024-01-05 18:13:19 +0000

    some cleaning

--- src/clearinghouse_objects.py
+++ src/clearinghouse_objects.py
@@ -8,64 +8,64 @@ __docformat___ = 'reStructuredText'
 
 """
 import json
+import os.path
 
 from icecream import ic
-import os.path
-import sys
+
 
 class NewSampleCuration:
     """ NewSampleCuration
         a class for preparing new annotation for submission to the Clearing house
         see the PDF details in https://www.ebi.ac.uk/ena/clearinghouse/api/
       """
-    def __init__(self,useENAAutoCurationValues):
+
+    def __init__(self, useENAAutoCurationValues):
         ic()
-        self.assertionAdditionalInfo = ""
-        #                           'assertionEvidences': [{'label': ''}],
-        self.assertionEvidences = []   #optional free text
-        self.assertionSource = ""   #optional Valid URI  e.g. to a publication
-        self.assertionMethod = ""    #mandatory child term of ECO_0000217 assertion method
-        self.attributeDelete = ""    #optional
-        self.attributePost = ""      #optional, but at least one of Post or Pre needed
-        self.attributePre = ""       #optional, but at least one of Post or Pre needed
-        self.valuePost = ""          #optional
-        self.valuePre = ""          #optional
-        self.providerName = ""  #Mandatory
-        self.providerUrl = "" #optional
-        self.recordId = ""              #mandatory
-        self.recordType = 'sample'      #mandatory
-        self.dataType = ""  #optional , CV
+        self.recordId = ""  # mandatory
+        self.recordType = 'sample'  # mandatory
+        self.dataType = ""  # optional , CV
         self.dataIdentifier = ""  # optional, free text
+        self.providerName = ""  # Mandatory, see useENAAutoCurationValues
+        self.providerUrl = ""  # optional, see useENAAutoCurationValues
+
+        self.assertionEvidences = []  # optional free text, see putAssertionEvidence
+        self.assertionSource = ""  # optional Valid URI  e.g. to a publication
+        self.assertionMethod = ""  # mandatory child term of ECO_0000217 assertion method
+        self.assertionAdditionalInfo = ""
+
+        self.attributeDelete = ""  # optional
+        self.attributePost = ""  # optional, but at least one of Post or Pre needed
+        self.attributePre = ""  # optional, but at least one of Post or Pre needed
+        self.valuePost = ""  # optional
+        self.valuePre = ""  # optional
+
         if useENAAutoCurationValues:
             self.assertionMethod = "automatic assertion"  # mandatory child term of ECO_0000217 assertion method
             self.providerName = "European Nucleotide Archive"  # Mandatory
             self.providerUrl = "https://www.ebi.ac.uk/ena/browser/home"  # optional
             self.addAutoAssertionEvidence("void")
 
-
     def emptyAssertionEvidence(self):
         self.assertionEvidences = []
 
     def putAssertionEvidence(self, identifier_value, label_value):
         """ putAssertionEvidence
-        https://www.ebi.ac.uk/ols/ontologies/eco
+               https://www.ebi.ac.uk/ols/ontologies/eco
+              'assertionEvidences': [{'label': ''}],
         :param identifier_value:
-        :param label_valye:
+        :param label_value:
         :return:
         """
-        #identifier is from ECO_0006019 child
+        # identifier is from ECO_0006019 child
         # label is from ECO_0006019 child
-        #provide id or label
-        evidence = {}
-        evidence["identifier"] = identifier_value
-        evidence["label"] = label_value
+        # provide id or label
+        evidence = {"identifier": identifier_value, "label": label_value}
         self.assertionEvidences.append(evidence)
 
     def addAutoAssertionEvidence(self, extra_evidence):
-        """addAutoAssertionEvidence adds the identifier and label for the evidence.
-        the identifier and label are from https://www.ebi.ac.uk/ols/ontologies/eco native=https://www.evidenceontology.org/
-        multiple of these pairs are allowed per curation
-        :return:
+        """addAutoAssertionEvidence adds the identifier and label for the evidence. the identifier and label are from
+        https://www.ebi.ac.uk/ols/ontologies/eco native=https://www.evidenceontology.org/ multiple of these pairs are
+        allowed per curation :extra_evidence: string of values ['combinatorial']  optional!
         """
         identifier = "ECO:0000203"
         label = "An assertion method that does not involve human review."
@@ -74,7 +74,8 @@ class NewSampleCuration:
         # currently all evidence is based on GPS coordinate hits to shapefiles or taxonomy inference
         # it would be useful if there was evidence code from spatial coordinates
         identifier = "ECO:0000366"
-        label = "A type of evidence based on logical inference from an automatically curated annotation that is used in an automatic assertion."
+        label = ("A type of evidence based on logical inference from an automatically curated annotation that is used "
+                 "in an automatic assertion.")
         self.putAssertionEvidence(identifier, label)
 
         # ic(extra_evidence)
@@ -85,24 +86,16 @@ class NewSampleCuration:
             label = "automatically integrated combinatorial computational evidence used in automatic assertion"
             self.putAssertionEvidence(identifier, label)
 
-        return
-
     def get_filled_dict(self):
         """get_filled_dict
 
         :return: a dictionary of the new curation record
         """
-        my_dict = {}
-        my_dict['recordId'] = self.recordId
-        my_dict['recordType'] = self.recordType
-        my_dict['assertionEvidences'] = self.assertionEvidences
-        my_dict['assertionAdditionalInfo'] = self.assertionAdditionalInfo
-        my_dict['assertionSource'] = self.assertionSource
-        my_dict['assertionMethod'] = self.assertionMethod
-        my_dict['attributePost'] = self.attributePost
-        my_dict['valuePost'] = self.valuePost
-        my_dict['providerName'] = self.providerName
-        my_dict['providerUrl'] = self.providerUrl
+        my_dict = {'recordId': self.recordId, 'recordType': self.recordType,
+                   'assertionEvidences': self.assertionEvidences,
+                   'assertionAdditionalInfo': self.assertionAdditionalInfo, 'assertionSource': self.assertionSource,
+                   'assertionMethod': self.assertionMethod, 'attributePost': self.attributePost,
+                   'valuePost': self.valuePost, 'providerName': self.providerName, 'providerUrl': self.providerUrl}
 
         return my_dict
 
@@ -111,10 +104,7 @@ class NewSampleCuration:
         calls converts the dictionary to JSON string
         :return: json
         """""
-
         return json.dumps(self.get_filled_dict(), indent = 4)
-
-
 
 
 def _get_template(template_type):
@@ -123,10 +113,9 @@ def _get_template(template_type):
     :return: empty_dict
     """
     template_file = ""
-    if (template_type == "sample"):
+    if template_type == "sample":
         template_file = "empty_clearinghouse_submission_template.json"
 
-    empty_dict = {}
     if os.path.isfile(template_file):
         f = open(template_file)
         empty_dict = json.load(f)
@@ -137,10 +126,11 @@ def _get_template(template_type):
         ic(f"ERROR: {template_file} dies not exist")
         quit(1)
 
-    return(empty_dict)
+    return empty_dict
 
 
 def demo_format(test_status):
+    ic(test_status)
     sample_dict = _get_template("sample")
     ic(sample_dict)
     curations = sample_dict['curations']
@@ -148,8 +138,7 @@ def demo_format(test_status):
     curation_template_record = curations[0].copy()
     ic(curation_template_record)
 
-    curation_count = 0
-    my_record = NewSampleCuration(useENAAutoCurationValues=True)
+    my_record = NewSampleCuration(useENAAutoCurationValues = True)
     ic(my_record.get_filled_dict())
     sample_id = 'SAMD'
     ic(my_record.attributeDelete)
@@ -157,16 +146,11 @@ def demo_format(test_status):
     ic(my_record.get_filled_dict())
 
 
-
 def main():
     test_status = True
     demo_format(test_status)
-
-    #environment_biome.pickle
-
-
+    # environment_biome.pickle
 
 if __name__ == '__main__':
     ic()
-
     main()

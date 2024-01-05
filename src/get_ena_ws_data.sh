ea0aec47c32c927957be4d2abf1c1e918c6668f7
woollard <woollard@ebi.ac.uk>
2024-01-05 13:26:42 +0000

    since the advent of elastic search, some existing searches needed slightlty different parameters or returned fields in a different order

--- src/get_ena_ws_data.sh
+++ src/get_ena_ws_data.sh
@@ -10,16 +10,17 @@ cd $sample_dir || exit
 #the most used file N.B. this takes 90 minutes to download
 outfile="sample_much_raw.tsv"
 echo "From ENA, extract lots of useful columns of data as well as lat lon column: " $outfile
-curl -X GET "https://www.ebi.ac.uk/ena/portal/api/search?dataPortal=ena&dccDataOnly=false&download=false&fields=accession%2C%20secondary_sample_accession%2C%20description%2C%20checklist%2C%20collection_date%2C%20collection_date_submitted%2C%20tax_id%2C%20taxonomic_classification%2C%20lat%2C%20%20lon%2C%20country%2C%20depth%2C%20altitude%2C%20elevation%2C%20salinity%2C%20environment_biome%2C%20environment_feature%2C%20environment_material&includeMetagenomes=true&limit=0&result=sample&sortDirection=asc" -H "accept: */*" > $outfile
-
+curl -X GET "https://www.ebi.ac.uk/ena/portal/api/search?dataPortal=ena&dccDataOnly=false&download=false&fields=accession%2C%20secondary_sample_accession%2C%20description%2C%20checklist%2C%20collection_date%2C%20first_public%2C%20tax_id%2C%20taxonomic_classification%2C%20lat%2C%20%20lon%2C%20country%2C%20depth%2C%20altitude%2C%20elevation%2C%20salinity%2C%20environment_biome%2C%20environment_feature%2C%20environment_material&includeMetagenomes=true&limit=0&result=sample" -H "accept: */*" > $outfile
+# cat ../data/samples/downloads/sample_much_raw.tsv | awk -F ',' 'NR == 1 || $9 != "" {print}' | head -3  | cat -n
 outfile="all_sample_lat_lon_country.tsv"
 echo "From ENA, extract all lat lon column needed for the shapefile: " $outfile
-#curl -X GET "https://www.ebi.ac.uk/ena/portal/api/search?dataPortal=ena&dccDataOnly=false&download=false&fields=lon%2Clat%2Ccountry&includeMetagenomes=true&limit=0&result=sample&sortDirection=asc" -H "accept: */*" > $outfile
+curl -X GET "https://www.ebi.ac.uk/ena/portal/api/search?dataPortal=ena&dccDataOnly=false&download=false&fields=lon%2Clat%2Ccountry&includeMetagenomes=true&limit=0&result=sample" -H "accept: */*" > $outfile
 infile=$outfile
 echo "creating "$outfile" from "$infile
 outfile="all_sample_lat_longs_present_uniq.tsv"
-head -1 $infile | cut -f 2,3 > $outfile
-tail +2 $infile | cut -f 2,3 | sed '/^\t$/d' | sort | uniq >> $outfile
+head -1 $infile | cut -f 1,2 > $outfile
+tail +2 $infile | cut -f 1,2 | sed '/^\t$/d' | sort | uniq >> $outfile
+
 
 #informative fields
 outfile="all_sample_rtn_fields.txt"
@@ -37,4 +38,4 @@ curl -X GET 'https://www.ebi.ac.uk/ena/portal/api/returnFields?result=study'  -H
 
 outfile="ena_sample_species.txt"
 echo "All the species in ENA tax id and scientific_name"
-curl -X GET "https://www.ebi.ac.uk/ena/portal/api/search?dataPortal=ena&dccDataOnly=false&download=false&fields=tax_id%2Cscientific_name&includeMetagenomes=true&result=sample&sortDirection=asc" -H "accept: */*" | cut -f 2-3 | uniq | sort | uniq > $outfile
+curl -X GET "https://www.ebi.ac.uk/ena/portal/api/search?dataPortal=ena&dccDataOnly=false&download=false&fields=tax_id%2Cscientific_name&includeMetagenomes=true&result=sample" -H "accept: */*" | cut -f 2-3 | uniq | sort | uniq > $outfile

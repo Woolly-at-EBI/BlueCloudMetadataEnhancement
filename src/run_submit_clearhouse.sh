0ed44483a9621f3f0f4afcdcba93a55adc88071f
woollard <woollard@ebi.ac.uk>
2024-01-22 16:28:26 +0000

    added more stability

--- src/run_submit_clearhouse.sh
+++ src/run_submit_clearhouse.sh
@@ -32,12 +32,14 @@ if ! { [ ${submission_dir} != "" ] &&  [ -d $submission_dir ]; } ; then
       echo "${submission_dir}<--is not a valid directory, so exiting"
       exit
     fi
+    exit
 fi
 echo "INFO: commandline parameters are apparently correct (well done you!)"
 
 if [ ${mode} == "test" ]; then
   echo "INFO: using -->test<--- credentials etc."
   url="https://wwwdev.ebi.ac.uk/ena/clearinghouse/api/curations"
+  curation_server_api="https://wwwdev.ebi.ac.uk/ena/clearinghouse/api/swagger-ui/index.html"
   auth_url='https://explore.api.aai.ebi.ac.uk/auth'
   creds=$aai_test2_creds
   # submission_dir="/Users/woollard/projects/bluecloud/clearinghouse/submission_data/test/splits/"
@@ -46,6 +48,7 @@ else
   #PROD
   echo "using production credentials and setup"
   url="https://www.ebi.ac.uk/ena/clearinghouse/api/curations"
+  curation_server_api="https://www.ebi.ac.uk/ena/clearinghouse/api/swagger-ui/index.html"
   auth_url='https://api.aai.ebi.ac.uk/auth'
   creds=$aai_prod_creds
 #  submission_dir="/Users/woollard/projects/bluecloud/clearinghouse/submission_data/full/splits/"
@@ -59,16 +62,13 @@ if [ ${original_submission_dir} != ${submission_dir} ]; then
   echo "WARNING: submission_dir is not what was passed to commandline, currently: ${submission_dir}"
 fi
 
-exit
 export bearer_file="bearer_file"
 # echo $creds
-# if meed to renew the bearer ( the below is for the test):
+# if need to renew the bearer ( the below is for the test):
 #curl 'https://explore.api.aai.ebi.ac.uk/auth'  -u $aai_test2_creds 1> bearer_file 2>/dev/null
 #re-running the bearerkey frequently as it times out within an hour
 
-
-##################################################################################################
-#
+#################################################################################################
 
 function re_run_bearer_file () {
   auth_url=$1
@@ -84,38 +84,42 @@ function re_run_bearer_file () {
   fi
   }
 
-re_run_bearer_file $auth_url $creds
-export bearerkey=`cat $bearer_file`
-export bearer="Authorization: Bearer $bearerkey"
-
 function submit_2_clearinghouse () {
   export curation_json_file=$1
   echo $curation_json_file
   export bearerkey=`cat $bearer_file`
   export bearer="Authorization: Bearer $bearerkey"
   #echo $bearer
-  # -T is needed for big files -d @ is slightly faster and puts it into memoru
+  # -T is needed for big files -d @ is slightly faster and puts it into memory
   cmd=`echo curl -X POST \"${url}\" -H \"accept: */*\"  -H \"Content-Type: application/json\"   -H \"${bearer}\"  -T ${curation_json_file}`
-  cmd=`echo curl -X POST \"${url}\" -H \"accept: */*\"  -H \"Content-Type: application/json\"   -H \"${bearer}\"  -d @${curation_json_file}`
+  # cmd=`echo curl -X POST \"${url}\" -H \"accept: */*\"  -H \"Content-Type: application/json\"   -H \"${bearer}\"  -d @${curation_json_file}`
   #could not get to both see the command and execute it, so doing the dirty way via a new file
   echo $cmd
   echo $cmd > run_me.sh
   time sh ./run_me.sh
   echo " "
-
 }
+#################################################################################################
+## starting now to actually run stuff
+re_run_bearer_file $auth_url $creds
+export bearerkey=`cat $bearer_file`
+export bearer="Authorization: Bearer $bearerkey"
 
+file_counter=0
 for file in $submission_dir/*.json
 do
     echo $file
     echo $bearer
     submit_2_clearinghouse $file
-    sleep 0.1 # wait 0.1 seconds
+    sleep 0.1 # wait 0.1 seconds to give a break to the curl etc.
     re_run_bearer_file $auth_url $creds
+    file_counter=$[$file_counter +1]
 done
 
-
-echo $submission_dir | mail -s "submission finished" woollard@ebi.ac.uk
-#end of script
+out_message="submission_dir was ${submission_dir}\ncount of JSON files was: ${file_counter}"
+out_message="${out_message}\nsee curation_server_api: ${curation_server_api} and try out a some curation ids and also some sample ids you saw in the submission output"
+echo ${out_message}
+# echo $out_message| mail -s "Clearinghouse ${mode} submission finished" woollard@ebi.ac.uk
+echo "###########################end of script"
 
 

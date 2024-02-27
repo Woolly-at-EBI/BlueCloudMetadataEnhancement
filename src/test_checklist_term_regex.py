fe0ae2a9d2524c07bb36fa8a733774cb886195ed
woollard <woollard@ebi.ac.uk>
2024-02-27 19:18:34 +0000

    fixed typos

--- src/test_checklist_term_regex.py
+++ src/test_checklist_term_regex.py
@@ -7,38 +7,43 @@ __docformat___ = 'reStructuredText'
 chmod a+x test_checklist_term_regex.py
 """
 
-
 from icecream import ic
 import re
 
-def get_term_regex_dict():
 
+def get_term_regex_dict():
     term_regex_dict = {
         "simple_int": "[0-9]+",
         "broken_int": "[",
         "elevation": "[+-]?(0|((0\.)|([1-9][0-9]*\.?))[0-9]*)([Ee][+-]?[0-9]+)?",
         "elevation_new": "([+-]?(0|((0\.)|([1-9][0-9]*\.?))[0-9]*)([Ee][+-]?[0-9]+)?)|((^not collected$)|(^not provided$)|(^restricted access$)|(^missing: control sample$)|(^missing: sample group$)|(^missing: synthetic construct$)|(^missing: lab stock$)|(^missing: third party data$)|(^missing: data agreement established pre-2023$)|(^missing: endangered species$)|(^missing: human-identifiable$))",
         "test_regex": "([+-]?",
-        "altitude": "((0|((0\.)|([1-9][0-9]*\.?))[0-9]*)([Ee][+-]?[0-9]+)?)|((^not collected$)|(^not provided$)|(^restricted access$)|(^missing: control sample$)|(^missing: sample group$)|(^missing: synthetic construct$)|(^missing: lab stock$)|(^missing: third party data$)|(^missing: data agreement established pre-2023$)|(^missing: endangered species$)|(^missing: human-identifiable$))"
+        "altitude": "((0|((0\.)|([1-9][0-9]*\.?))[0-9]*)([Ee][+-]?[0-9]+)?)|((^not collected$)|(^not provided$)|("
+                    "^restricted access$)|(^missing: control sample$)|(^missing: sample group$)|(^missing: synthetic "
+                    "construct$)|(^missing: lab stock$)|(^missing: third party data$)|(^missing: data agreement "
+                    "established pre-2023$)|(^missing: endangered species$)|(^missing: human-identifiable$))",
+        "depth": "((0|((0\.)|([1-9][0-9]*\.?))[0-9]*)([Ee][+-]?[0-9]+)?)|((^not collected$)|(^not provided$)|("
+                 "^restricted access$)|(^missing: control sample$)|(^missing: sample group$)|(^missing: synthetic "
+                 "construct$)|(^missing: lab stock$)|(^missing: third party data$)|(^missing: data agreement "
+                 "established pre-2023$)|(^missing: endangered species$)|(^missing: human-identifiable$))"
     }
     return term_regex_dict
 
 
-def test_reqexs(term_regex_dict):
+def test_regexes(term_regex_dict):
     """"""
 
-    def is_valid_reqex(test_reqex):
+    def is_valid_regex(test_regex):
         try:
-            re.compile(test_reqex)
+            re.compile(test_regex)
             is_valid = True
         except re.error:
             is_valid = False
 
         return is_valid
 
-
     def simple_test(test_regex, test_string):
-        if not is_valid_reqex(test_regex):
+        if not is_valid_regex(test_regex):
             print(f"\tFalse : ->{test_string}<-  INVALID REGEX {test_regex}")
             return False
 
@@ -65,22 +70,24 @@ def test_reqexs(term_regex_dict):
         elif term == "altitude":
             simple_test(term_regex_dict[term], "99")
             simple_test(term_regex_dict[term], "missing: third party data")
-            simple_test(term_regex_dict[term], "missing: third party data ")
+        elif term == "depth":
+            simple_test(term_regex_dict[term], "99")
+            simple_test(term_regex_dict[term], "missing: third party data")
         else:
-            if is_valid_reqex(term_regex_dict[term]):
+            if is_valid_regex(term_regex_dict[term]):
                 print(f"\tTrue: ->{term}<-  VALID REGEX {term_regex_dict[term]}")
             else:
                 print(f"\tFalse: ->{term}<-  INVALID REGEX {term_regex_dict[term]}")
 
-
         ic("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
 
     ic()
 
+
 def main():
     term_regex_dict = get_term_regex_dict()
-    test_reqexs(term_regex_dict)
+    test_regexes(term_regex_dict)
+
 
 if __name__ == '__main__':
     ic()

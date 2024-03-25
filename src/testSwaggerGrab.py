28d0a2c762e001900b14afaaecc889066d4bc623
woollard <woollard@ebi.ac.uk>
2024-03-25 13:59:33 +0000

    removed icecream references

--- src/testSwaggerGrab.py
+++ src/testSwaggerGrab.py
@@ -8,7 +8,6 @@ __docformat___ = 'reStructuredText'
 chmod a+x testSwaggerGrab.py
 """
 
-from icecream import ic
 import sys
 import argparse
 import requests
@@ -33,12 +32,11 @@ def run_query(attribute_name, offset, limit):
         # 'cache-control': "no-cache"
     }
     query = get_query_params(attribute_name, offset, limit)
-    ic(f"{cho_url}, headers = {headers}, params = {query}")
+    print(f"{cho_url}, headers = {headers}, params = {query}")
     response = requests.request("GET", cho_url, headers = headers, params = query)
-    ic()
 
     if requests.codes.ok != 200:
-        ic(f"warning {requests.codes.ok} will try again in a few seconds")
+        print(f"warning {requests.codes.ok} will try again in a few seconds")
         time.sleep(5)  # sleep 5 seconds
         response = requests.request("GET", cho_url, headers = headers, params = query)
         if requests.codes.ok != 200:
@@ -51,15 +49,14 @@ def run_query(attribute_name, offset, limit):
 
 def do_cho_queries(attribute_name, output_json_file, passed_offset_start, limit_max, batch_size):
     """"""
-    ic()
     offset = passed_offset_start
     my_records = run_query(attribute_name, offset, 1)
     totalAttributes = my_records['totalAttributes']
-    ic(totalAttributes)
+    print(f"totalAttributes={totalAttributes}")
 
     # totalAttributes = 3003
 
-    ic(f"writing to {output_json_file}")
+    print(f"writing to {output_json_file}")
     if os.path.isfile(output_json_file):
         of_obj = open(output_json_file, "a")
     else:
@@ -71,16 +68,16 @@ def do_cho_queries(attribute_name, output_json_file, passed_offset_start, limit_
         limit_max = totalAttributes
     off_set_start = offset
     # off_set_start = 377500
-    ic(f"writing to {output_json_file} offset {off_set_start}")
+    print(f"writing to {output_json_file} offset {off_set_start}")
     #      got to 15:30 on 29th Feb '377500 of 532401'
     #     '453000 of 532401' 1t 17:02
     # off_set_start = 453000
     # off_set_start = 532000
     # off_set_start = 532399
 
-    ic(f"{off_set_start}, {limit_max}, {offset_size}")
+    print(f"{off_set_start}, {limit_max}, {offset_size}")
     for offset in range(off_set_start, limit_max, offset_size):
-        ic(f"{offset} of {totalAttributes}")
+        print(f"{offset} of {totalAttributes}")
         if (offset + limit) > totalAttributes:  # don't want to query more than there are
             limit = totalAttributes - offset
         my_records = run_query(attribute_name, offset, limit)
@@ -95,7 +92,6 @@ def main(attribute_name, outfile, offset_start, limit_max, batch_size):
 
 
 if __name__ == '__main__':
-    ic()
     # Read arguments from command line
     prog_des = "Script to extract all the curation records"
     parser = argparse.ArgumentParser(description = prog_des)
@@ -110,7 +106,6 @@ if __name__ == '__main__':
                         required = False, default = 500)
     parser.parse_args()
     args = parser.parse_args()
-    ic(args)
     print(args)
 
     main(args.attribute_name, args.outfile, args.offset_start, args.limit_max, args.batch_size)
